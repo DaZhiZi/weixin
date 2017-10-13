6ba1c972a8f80a47455dcd29000bfd8a4f66fa77
DESKTOP-FKHTLK3\yongzhi <751590100@qq.com>
2017-10-13 22:04:05 +0800

    2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var session = require('cookie-session')
 var moment = require('moment')
 let sha1 = require('sha1')
+var wechat = require('wechat');
 
 var date = moment().format('HH:mm:ss')
 var log = console.log.bind(console, date + ': ')
@@ -34,8 +35,13 @@ let config = {
         appsecret: 'c1c19e6d83ec0550f7865dfb79accbd0',
         //这里你得填写你自己设置的Token值
         token    : 'yongzhi',
-    }
+    },
+    token: 'yongzhi',
+    appid: 'wx32f55c167a333096',
+    encodingAESKey: 'encodinAESKey',
+    checkSignature: true // 可选，默认为true。
 }
+app.use(express.query());
 
 app.use(function (request, response) {
     //ctx.query获取请求中携带的参数
@@ -62,6 +68,43 @@ app.use(function (request, response) {
     }
 })
 
+app.use('/', wechat(config, function (req, res, next) {
+    // 微信输入信息都在req.weixin上
+    var message = req.weixin;
+    if (message.FromUserName === 'diaosi') {
+        // 回复屌丝(普通回复)
+        res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+        //你也可以这样回复text类型的信息
+        res.reply({
+            content: 'text object',
+            type: 'text'
+        });
+    } else if (message.FromUserName === 'hehe') {
+        // 回复一段音乐
+        res.reply({
+            type: "music",
+            content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3",
+                thumbMediaId: "thisThumbMediaId"
+            }
+        });
+    } else {
+        // 回复高富帅(图文回复)
+        res.reply([
+            {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+            }
+        ]);
+    }
+}));
+
 const registerRoutes = (app, routes) => {
     for (var i = 0; i < routes.length; i++) {
         var route = routes[i]
@@ -79,7 +122,7 @@ var _routeFunc = function () {
     }
 }
 
-_routeFunc()
+//_routeFunc()
 
 
 var server = app.listen(3000, function () {
