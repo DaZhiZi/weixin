71199b5e029872c77f959ee1a096730def6f1faf
DESKTOP-FKHTLK3\yongzhi <751590100@qq.com>
2017-10-13 22:43:20 +0800

    2

--- app.js
+++ app.js
@@ -41,44 +41,42 @@ let config = {
     checkSignature: true // 可选，默认为true。
 }
 app.use(express.query());
-
-app.use(function (request, response, next) {
-    //ctx.query获取请求中携带的参数
-    let token = config.wechat.token
-    //获取请求中的signature
-    let signature = request.query.signature
-    //获取请求中的timestamp
-    let timestamp = request.query.timestamp
-    //获取请求中的nonce
-    let nonce = request.query.nonce
-    //获取请求中的echostr
-    let echostr = request.query.echostr
-    //将Token，timestamp，nonce按字典排序,排序后链接成一个字符串
-    let str = [token, timestamp, nonce].sort().join('')
-    //使用sha1模块进行sha1加密
-    let sha1Str = sha1(str)
-    //判断加密后的字符串与请求中signature是否相等
-    //如果相等返回echostr
-    if (sha1Str === signature) {
-        //将echostr返回给微信服务器
-        //response.send(echostr)
-    } else {
-        //response.send("不是来自微信的请求")
-    }
-    next()
-})
-
+//需要去掉微信验证，要不然res.reply();两次，导致error
 app.use('*', wechat(config, function (req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
     log('message', message)
-    if (message.Content === 'qq') {
-        log('前面')
+    if (message.Content === 'diaosi') {
         // 回复屌丝(普通回复)
         res.reply('hehe');
-        log('hehe之后')
+    } else if (message.FromUserName === 'text') {
+        //你也可以这样回复text类型的信息
+        res.reply({
+            content: 'text object',
+            type: 'text'
+        });
+    } else if (message.Content === 'hehe') {
+        // 回复一段音乐
+        res.reply({
+            type: "music",
+            content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3",
+                thumbMediaId: "thisThumbMediaId"
+            }
+        });
     } else {
-        res.reply('ooooo');
+        // 回复高富帅(图文回复)
+        res.reply([
+            {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+            }
+        ]);
     }
 }));
 
